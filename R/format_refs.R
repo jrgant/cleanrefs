76288a61093ab8233b6bb36cbe75501c4e777dee
jrgant <jason.gantenberg@gmail.com>
2019-05-24 09:10:19 -0400

    add write parameter

--- R/format_refs.R
+++ R/format_refs.R
@@ -5,43 +5,50 @@
 #'
 #' @param refdat Name of CSV file containing Paperpile export (without .csv extension).
 #' @param indir Directory containing Paperile CSV export file.
-#' @param outdir Directory to write reformatted CSV file.
+#' @param outdir Directory to write reformatted CSV file. If not specified, function will use `indir` as default write directory.
+#' @param write Write formatted references to CSV (default: TRUE).
 #'
 #' @import dplyr
+#' @import stringr
 #' @importFrom readr read_csv
 #'
 #' @export format_refs
 
-format_refs <- function(refdat, indir, outdir = NULL) {
+format_refs <- function(refdat, indir, outdir = NULL, write = TRUE) {
 
   file <- paste0(indir, "/", refdat, ".csv")
 
   dat <-
     read_csv(file) %>%
     select(
-      "auth" = "Authors",
-      "title" = "Title",
-      "jnl" = "Journal",
-      "yr" = "Publication year",
-      "vol" = "Volume",
-      "iss" = "Issue",
-      "pgs" = "Pages",
-      "doi" = "DOI",
-      "pmid" = "PMID",
-      "pmcid" = "PMC ID",
-      "abs" = "Abstract"
-      )
+      "Title",
+      "Journal",
+      "Pages",
+      "Volume",
+      "Issue",
+      "Publication year",
+      "DOI",
+      "Abstract",
+      "Authors",
+      ) %>%
+    mutate(
+      Journal = str_remove_all(Journal, "\\."),
+      `First author` = str_extract(Authors, "^[A-Za-z]+"),
+      Authors = str_replace_all(Authors, "\\,", "\\, "),
+      Pages = str_replace(Pages, "\\-", "\\-\\-")
+      ) %>%
+    select(Title, `First author`, everything())
 
   if (is.null(outdir)) {
     flout <- paste0(indir, "/", refdat, "_reformat", ".csv")
-    write.csv(dat, flout, row.names = F)
-    read_csv(flout)
 
   } else {
     flout <- paste0(indir, "/", refdat, "_reformat", ".csv")
-    write.csv(dat, flout, row.names = F)
-    read_csv(flout)
   }
 
+  if (write) {
+    write.csv(dat, flout, row.names = F)
+  }
 
+  read_csv(flout)
 }
